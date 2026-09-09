7ebc7ec2ba8df440731ab313b8a7a3cd047372e1
Maxime Rivest <mrive052@gmail.com>
2026-09-09 14:31:25 -0400

    Remove copied lm15 before establishing its subtree

--- dspy/_vendor/lm15/model_registry.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility wrapper for the model registry API.
-
-ModelRegistry lives in lm15.models alongside ModelInfo. This module exists for
-users who prefer importing it from lm15.model_registry.
-"""
-
-from .models import ModelRegistry
-
-__all__ = ["ModelRegistry"]
